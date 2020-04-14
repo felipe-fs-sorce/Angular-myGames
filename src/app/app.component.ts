833ef47fd2b23e2e9f3223922333a4425b550404
Felipe Silva <52787990+felipe-fs-sorce@users.noreply.github.com>
2020-04-13 22:08:47 -0300

    Meu segundo envio
    Atividade 2 - Firebase já conectado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myGames';
+
+
+  // Variável assíncrona
+  items: Observable<any[]>;
+
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('users').valueChanges();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCCqVP4dSPJ8A9fBlOvcsluyOFbWOL7tI0',
+    authDomain: 'my-games-5aeb8.firebaseapp.com',
+    databaseURL: 'https://my-games-5aeb8.firebaseio.com',
+    projectId: 'my-games-5aeb8',
+    storageBucket: 'my-games-5aeb8.appspot.com',
+    messagingSenderId: '158323965293',
+    appId: '1:158323965293:web:e5c6ec5a970dcbd2692e64'
+  }
 };
 
 /*
