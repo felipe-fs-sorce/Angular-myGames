7fa630f6f737b11ac8db5c2fb08c2c0bad32cfd2
Felipe Silva <52787990+felipe-fs-sorce@users.noreply.github.com>
2020-04-16 02:19:35 -0300

    Atualização do template aula 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,89 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs/operators';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myGames';
+export class AppComponent implements OnInit {
 
+pageTitle = 'My Games';
 
-  // Variável assíncrona
-  items: Observable<any[]>;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private titleService: Title
+  ) { }
 
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('users').valueChanges();
+  changeTitle() {
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let child = this.activatedRoute.firstChild;
+        while (child) {
+          if(child.firstChild) {
+            child = child.firstChild;
+          } else if (child.snapshot.data && child.snapshot.data.title) {
+            return`${child.snapshot.data.title} ~ ${this.pageTitle}`;
+          }else {
+            return this.pageTitle;
+          }
+
+        }
+        return this.pageTitle;
+      })).subscribe((title: any) => {
+        this.titleService.setTitle(title);
+      });
+
   }
+
+  menutoggle() {
+    $(document).ready(() => {
+
+      if( $('#links').is(':visible') ) {
+        this.menuHide();
+      } else {
+        this.menuShow();
+      }
+
+    });
+    return false;
+  }
+
+  menuHide() {
+    $('#links').slideUp('fast');
+  }
+
+  menuShow() {
+    $('#links').slideDown('fast');
+  }
+
+  ngOnInit(): void {
+    this.changeTitle();
+
+    $(document).ready(() => {
+
+
+      $(window).resize(() => {
+        if (window.innerWidth > 511) {
+          $('#links').show(0);
+        } else {
+          $('#links').hide(0);
+        }
+      });
+
+
+      $(document).on('click', '#links > a', () => {
+        $('#links').slideUp('fast');
+      });
+
+    });
+
+  }
+
 }
